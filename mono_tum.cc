926e2146ebb1017626ffdd10b5a0c1c9d86ed259
Yanir <yanir.edri1@gmail.com>
2021-09-02 20:23:01 +0300

    update mono_tum.cc

--- mono_tum.cc
+++ mono_tum.cc
@@ -1,184 +1,169 @@
 /**
-* This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of ORB-SLAM2.
+ *
+ * Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University
+ * of Zaragoza) For more information see <https://github.com/raulmur/ORB_SLAM2>
+ *
+ * ORB-SLAM2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ORB-SLAM2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-
-#include <iostream>
-#include <algorithm>
-#include <fstream>
-#include <chrono>
-#include <unistd.h>	
-#include <opencv2/core/core.hpp>
-
-#include <ctello.h>
+#include <Converter.h>
 #include <System.h>
-#include <Converter.h>	
+#include <ctello.h>
+#include <unistd.h>
+#include <signal.h>
+#include <algorithm>
+#include <atomic>
+#include <chrono>
+#include <fstream>
+#include <future>
+#include <iostream>
+#include <mutex>
+#include <opencv2/core/core.hpp>
+#include <thread>
+
 #include "opencv2/opencv.hpp"
 using namespace std;
 using namespace chrono;
 using namespace cv;
 using namespace ctello;
+// Scan Rotation Angle
+#define sra 30
+// Scan Vertical Movement
+#define svm 20
 
+atomic<int> scanFinished(0);
+atomic<int> droneLocalized(0);
 
-void saveMap(ORB_SLAM2::System SLAM){
-    std::vector<ORB_SLAM2::MapPoint*> mapPoints = SLAM.GetMap()->GetAllMapPoints();
+void commandTello(Tello &tello, string com, int dur = 0) {
+    cout << "sending command " << com << endl;
+    tello.SendCommand(com);
+    while (!tello.ReceiveResponse());
+    if (dur > 0) sleep(dur);
+}
+void moveTello(Tello &tello, string dir, int x, int dur = 0) {
+    commandTello(tello, dir + " " + to_string(x), dur);
+}
+void takeOff(Tello &tello) {
+    tello.SendCommand("takeoff");
+    sleep(1);
+    while (!(tello.ReceiveResponse()));
+}
+void land(Tello &tello) { commandTello(tello, "land"); }
+void scan(Tello &tello) {
+    int angle = 0, v = sra;
+    while (angle < 360) {
+        if (droneLocalized.load()) {
+            moveTello(tello, "cw", v, 1);
+            angle += v;
+            v = (v + sra) / 2;
+        } else {
+            moveTello(tello, "ccw", v, 1);
+            angle -= v;
+            v = v / 2;
+            if (v <= 10) v = 10;
+        }
+        cout << "angle is now " << angle << endl;
+        moveTello(tello, "up", svm, 1);
+        moveTello(tello, "down", svm, 1);
+    }
+    // for now, land immeidiatly
+    land(tello);
+    scanFinished.store(1);
+}
+
+void saveMap(ORB_SLAM2::System &SLAM) {
+    std::vector<ORB_SLAM2::MapPoint *> mapPoints =
+        SLAM.GetMap()->GetAllMapPoints();
     std::ofstream pointData;
     pointData.open("/tmp/pointData.csv");
-    for(auto p : mapPoints) {
-        if (p != NULL)
-        {
+    for (auto p : mapPoints) {
+        if (p != NULL) {
             auto point = p->GetWorldPos();
-            Eigen::Matrix<double, 3, 1> v = ORB_SLAM2::Converter::toVector3d(point);
-            pointData << v.x() << "," << v.y() << "," << v.z()<<  std::endl;
+            Eigen::Matrix<double, 3, 1> v =
+                ORB_SLAM2::Converter::toVector3d(point);
+            pointData << v.x() << "," << v.y() << "," << v.z() << std::endl;
         }
     }
     pointData.close();
 }
 
+int ret = 0, finish = 0, scanCalled = 0;
+cv::Mat im;
+double t = 0;
 
+void takePicture() {
+    VideoCapture capture{"udp://0.0.0.0:11111?fifo_size=100000", cv::CAP_FFMPEG};
+    if (!capture.isOpened()) {
+        cerr << "BASA camera not opened" << endl;
+        exit(1);
+    }
+    while (!finish) {
+        capture >> im;
+        ret = 1;
+    }
+}
 
-
-void LoadImages(const string &strFile, vector<string> &vstrImageFilenames,
-                vector<double> &vTimestamps);
-
-int main(int argc, char **argv)
-{
-	cout << "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" << endl;
-    if(argc != 4)
-    {
-        cerr << endl << "Usage: ./mono_tum path_to_vocabulary path_to_settings path_to_sequence" << endl;
+int main(int argc, char **argv) {
+   cout << "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" << endl;
+    if (argc != 3) {
+        cerr << endl
+             << "Usage: ./mono_tum path_to_vocabulary path_to_settings" << endl;
         return 1;
     }
 
-    // Retrieve paths to images
-    vector<string> vstrImageFilenames;
-    vector<double> vTimestamps;
-    string strFile = string(argv[3])+"/rgb.txt";
-    LoadImages(strFile, vstrImageFilenames, vTimestamps);
-
-    int nImages = vstrImageFilenames.size();
-
-    // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
-
-    // Vector for tracking time statistics
-    vector<float> vTimesTrack;
-    vTimesTrack.resize(nImages);
-
-    cout << endl << "-------" << endl;
-    cout << "Start processing sequence ..." << endl;
-    cout << "Images in the sequence: " << nImages << endl << endl;
-	
     Tello tello;
     if (!tello.Bind()) return 0;
-    //tello.SendCommand("takeoff");
-    //while (!(tello.ReceiveResponse()));
-    //tello.SendCommand("land");
-    //while (!(tello.ReceiveResponse()));
+    tello.SendCommand("streamon");
+    thread picture_thread(takePicture);
+    // Create SLAM system. It initializes all system threads and gets ready to
+    // process frames.
+    ORB_SLAM2::System SLAM(argv[1], argv[2], ORB_SLAM2::System::MONOCULAR,
+                           true);
+    
+
+    cout << endl << "-------" << endl;
+    cout << "Start processing..." << endl;
     
     // Main loop
-    tello.SendCommand("streamon");
-    while (!(tello.ReceiveResponse()));
-    VideoCapture camera{{"udp://0.0.0.0:11111?overrun_nofatal=1&fifo_size=10000000"}, cv::CAP_ANY};
-    //VideoCapture camera(0);
-    camera.set(CV_CAP_PROP_BUFFERSIZE,1);
-    if(!camera.isOpened()) {
-    	cerr << "BASA camera not opened" << endl;
-    	exit(1);
-    }
-    cv::Mat im;
-    //tello.SendCommand("takeoff")
-    int _ni = 0;
-    while(1) {
-        // Read image from file
-        //im = cv::imread(string(argv[3])+"/"+vstrImageFilenames[ni],CV_LOAD_IMAGE_UNCHANGED);
-        
-        //for(int i = 0; i < 15; i++) camera.grab();
-        
-        camera.read(im);
-        //double tframe = camera.get(cv::CAP_PROP_POS_MSEC)/1000.0;  
-        //double tframe = vTimestamps[_ni++];      
-        if(im.empty()) {
+    takeOff(tello);
+    thread scan_thread;
+    while (!ret) usleep(5000);
+    int i = 0;
+    cv::Mat trackRet;
+    while (!scanFinished.load()) {
+        i++;
+        if (i % 10 != 0) usleep(5000);
+        if (im.empty()) {
             cerr << endl << "Failed to load image from camera :(" << endl;
             exit(1);
         }
-        //if(_ni++ % 1000 == 0) { cerr << "ROTATE NOW" << endl; }
-        SLAM.TrackMonocular(im,camera.get(cv::CAP_PROP_POS_MSEC)/1000.0); // Pass the image to the SLAM system
-
-        // Wait to load the next frame
-        /*double T=0;
-        if(ni<nImages-1)
-            T = vTimestamps[ni+1]-tframe;
-        else if(ni>0)
-            T = tframe-vTimestamps[ni-1];
-
-        if(ttrack<T)
-            usleep((T-ttrack)*1e6);*/
+        trackRet = SLAM.TrackMonocular(im, i);
+        droneLocalized.store(!trackRet.empty());
+        if (!trackRet.empty() && !scanCalled) {
+            scan_thread = thread(scan, std::ref(tello));
+            scanCalled = 1;
+        }
     }
-
+    finish = 1;
     // Stop all threads
-    // saveMap(SLAM);
+    saveMap(SLAM);
     SLAM.Shutdown();
-    //tello.SendCommand("streamoff");
-    nImages = _ni;
-    // Tracking time statistics
-    sort(vTimesTrack.begin(),vTimesTrack.end());
-    float totaltime = 0;
-    for(int ni=0; ni<nImages; ni++)
-    {
-        totaltime+=vTimesTrack[ni];
-    }
-    cout << "-------" << endl << endl;
-    cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
-    cout << "mean tracking time: " << totaltime/nImages << endl;
-
+    tello.SendCommand("streamoff");
+    // imageChoice = -100;
     // Save camera trajectory
     SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
 
     return 0;
-}
-
-void LoadImages(const string &strFile, vector<string> &vstrImageFilenames, vector<double> &vTimestamps)
-{
-    ifstream f;
-    f.open(strFile.c_str());
-
-    // skip first three lines
-    string s0;
-    getline(f,s0);
-    getline(f,s0);
-    getline(f,s0);
-
-    while(!f.eof())
-    {
-        string s;
-        getline(f,s);
-        if(!s.empty())
-        {
-            stringstream ss;
-            ss << s;
-            double t;
-            string sRGB;
-            ss >> t;
-            vTimestamps.push_back(t);
-            ss >> sRGB;
-            vstrImageFilenames.push_back(sRGB);
-        }
-    }
 }
